cd628e4356b7721c154f746458b2100de28f0024
김락현 <rlafkrgus963@kumoh.ac.kr>
2020-01-15 10:24:32 +0900

    [락현] S02P11D127-18, S02P11D127-19
    모바일 사이드 바, 네비게이션 바 완성

--- src/views/PortfolioPage.vue
+++ src/views/PortfolioPage.vue
@@ -1,17 +1,23 @@
 <template>
   <div>
+    <header>
+      <NavigationBar v-if="visialbe"></NavigationBar>
+      <NavigationBarMobile v-if="!visialbe"></NavigationBarMobile>
+    </header>
     <ImgBanner imgSrc="https://source.unsplash.com/5brvJbR1Pn8/1600x900">
-      <div style="line-height:1.2em;font-size:1.2em;" slot="text">Portfolio</div>
+      <div style="line-height:1.2em;font-size:1.2em;" slot="text">
+        Portfolio
+      </div>
     </ImgBanner>
     <v-container>
-
       <!-- Portfolio -->
       <v-layout>
-        <v-flex xs12>
-          <PortfolioList :limits="6" :load-more="true"></PortfolioList>
-        </v-flex>
+        <v-row v-resize="onResize" align="center" justify="center">
+          <v-flex xs12>
+            <PortfolioList :limits="6" :load-more="true"></PortfolioList>
+          </v-flex>
+        </v-row>
       </v-layout>
-
     </v-container>
   </div>
 </template>
@@ -19,12 +25,31 @@
 <script>
 import ImgBanner from '../components/ImgBanner'
 import PortfolioList from '../components/PortfolioList'
+import NavigationBar from '../components/NavigationBar'
+import NavigationBarMobile from '../components/NavigationBarMobile'
 
 export default {
-	name: 'PortfolioPage',
-	components: {
-		ImgBanner,
-		PortfolioList,
-	},
+  name: 'PortfolioPage',
+  data: () => ({
+    visialbe: false
+  }),
+  components: {
+    ImgBanner,
+    PortfolioList,
+    NavigationBar,
+    NavigationBarMobile
+  },
+  methods: {
+    onResize () {
+      if (window.innerWidth <= 576) {
+        this.visialbe = false
+      } else {
+        this.visialbe = true
+      }
+    }
+  },
+  mounted () {
+    this.onResize()
+  }
 }
 </script>
